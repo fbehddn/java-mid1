8bea50edf656a97b562c94098a4341f9d2d707d4
fbehddn <tom0611@naver.com>
2024-04-10 14:27:50 +0900

    chap1_Object 클래스_toString

---
+++ src/lang/tostring/ToStringMain1.java
@@ -0,0 +1,13 @@
+package lang.tostring;
+
+public class ToStringMain1 {
+    public static void main(String[] args) {
+        Object object = new Object();
+
+        //toString()의 반환값 출력
+        System.out.println(object.toString());
+
+        //object() 직접 출력
+        System.out.println(object);
+    }
+}
