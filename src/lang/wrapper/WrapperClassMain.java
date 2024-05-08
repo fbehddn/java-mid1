8507432561cfd101ec6889822f0011080e3fc25d
fbehddn <tom0611@naver.com>
2024-05-08 22:13:51 +0900

    chap4_래퍼클래스 - 자바 래퍼 클래스

---
+++ src/lang/wrapper/WrapperClassMain.java
@@ -0,0 +1,24 @@
+package lang.wrapper;
+
+public class WrapperClassMain {
+    public static void main(String[] args) {
+        Integer integer = new Integer(10);
+        Integer integerObj = Integer.valueOf(10);
+        Long longObj = Long.valueOf(100);
+        Double doubleObj = Double.valueOf(10.5);
+        System.out.println("integer = " + integer);
+        System.out.println("integerObj = " + integerObj);
+        System.out.println("longObj = " + longObj);
+        System.out.println("doubleObj = " + doubleObj);
+
+        System.out.println("내부 값 읽기");
+        int intValue = integerObj.intValue();
+        System.out.println("intValue = " + intValue);
+        long longValue = longObj.longValue();
+        System.out.println("longValue = " + longValue);
+
+        System.out.println("비교");
+        System.out.println("==: " + (integer == integerObj));
+        System.out.println("equals: " + integer.equals(integerObj));
+    }
+}
