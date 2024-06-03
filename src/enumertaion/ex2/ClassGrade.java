8b01ee4b44b86ab5daf1ae287c718d7f61f8d962
fbehddn <tom0611@naver.com>
2024-06-03 13:12:42 +0900

    chap5_열거형 - 타입 안전 열거형 패턴

---
+++ src/enumertaion/ex2/ClassGrade.java
@@ -0,0 +1,9 @@
+package enumertaion.ex2;
+
+public class ClassGrade {
+    public static final ClassGrade BASIC = new ClassGrade();
+    public static final ClassGrade GOLD = new ClassGrade();
+    public static final ClassGrade DIAMOND = new ClassGrade();
+
+    private ClassGrade() {}
+}
---
+++ src/enumertaion/ex2/ClassGradeEx2_2.java
@@ -0,0 +1,14 @@
+package enumertaion.ex2;
+
+public class ClassGradeEx2_2 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+/*
+        ClassGrade classGrade = new ClassGrade();
+        int discount = discountService.discount(classGrade, price);
+        System.out.println("discount = " + discount);
+*/
+    }
+}
