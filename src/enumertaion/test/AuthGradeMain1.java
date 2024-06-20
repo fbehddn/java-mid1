80891c209a15228a72e9320a59e0ab11eae217c3
fbehddn <tom0611@naver.com>
2024-06-20 22:25:19 +0900

    chap5_열거형 - 문제와 풀이1

---
+++ src/enumertaion/test/AuthGradeMain1.java
@@ -0,0 +1,11 @@
+package enumertaion.test;
+
+public class AuthGradeMain1 {
+    public static void main(String[] args) {
+
+        AuthGrade[] values = AuthGrade.values();
+        for (AuthGrade value : values) {
+            System.out.println("grade=" + value + ", level=" + value.getLevel() + ", 설명=" + value.getDescription());
+        }
+    }
+}
