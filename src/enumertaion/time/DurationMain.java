75afe7d39ea00c51f68e1034a056c9e32c72e24d
fbehddn <tom0611@naver.com>
2024-06-23 12:53:11 +0900

    chap6_날짜와 시간 - 기간,시간의 간격 - Duration,Period

---
+++ src/enumertaion/time/DurationMain.java
@@ -0,0 +1,24 @@
+package enumertaion.time;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class DurationMain {
+    public static void main(String[] args) {
+        Duration duration = Duration.ofMinutes(30);
+        System.out.println("duration = " + duration);
+
+        LocalTime lt = LocalTime.of(1, 0);
+        System.out.println("lt = " + lt);
+
+        LocalTime plus = lt.plus(duration);
+        System.out.println("plus = " + plus);
+
+        LocalTime start = LocalTime.of(9, 0);
+        LocalTime end = LocalTime.of(10, 0);
+        Duration between = Duration.between(start, end);
+        System.out.println("between = " + between);
+        System.out.println("차이:" + between.getSeconds() + "초");
+        System.out.println("근무시간:" + between.toHours() + "시간" + between.toMinutesPart() + "분");
+    }
+}
